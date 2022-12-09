1f66c210714a0af9fa8376a5717a77fd29a5acfb
AllaKulchitskaya <alla070@list.ru>
2022-12-09 12:50:20 +0300

    Дз 1 - Задание 1

---
+++ src/transport/Transport.java
@@ -0,0 +1,77 @@
+package transport;
+
+public class Transport {
+    private final String brand;
+    private final String model;
+    private final int year;
+    private final String country;
+    private String color;
+    private int maxSpeed;
+
+    public Transport(String brand, String model, int year, String country, String color) {
+        this(brand, model, year, country, color, 120);
+    }
+
+    public Transport(String brand, String model, int year, String country, String color, int maxSpeed) {
+        if (brand == null || brand.isEmpty() || brand.isBlank()) {
+            this.brand = "default";
+        } else {
+            this.brand = brand;
+        }
+        if (model == null || model.isEmpty() || model.isBlank()) {
+            this.model = "default";
+        } else {
+            this.model = model;
+        }
+        if (year <= 0) {
+            this.year = 2000;
+        } else {
+            this.year = year;
+        }
+        if (country == null || country.isEmpty() || country.isBlank()) {
+            this.country = "default";
+        } else {
+            this.country = country;
+        }
+        setColor(color);
+        setMaxSpeed(maxSpeed);
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+        if (this.color == null || this.color.isEmpty() || this.color.isBlank()) {
+            this.color = "белый";
+        }
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setMaxSpeed(int maxSpeed) {
+        this.maxSpeed = maxSpeed;
+        if (this.maxSpeed <= 0) {
+            this.maxSpeed = 120;
+        }
+    }
+}
